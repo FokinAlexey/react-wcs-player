149f498919d8386ca1b7e28c6edd5942a92d9b7d
FokinAlexey <a.fokin1979@gmail.com>
2021-02-12 18:50:24 +0300

    ddd

--- src/Components/data.js
+++ src/Components/data.js
@@ -1,24 +1,24 @@
 const data = [
     {
-        imgUrl: 'https://i.ytimg.com/an_webp/XSs2cR2Tvuk/mqdefault_6s_480x270.webp?du=3000&sqp=CMe5mIEG&rs=AOn4CLAJbLTAxwoo82nXCMXDezVlsGgXYw',
+        imgUrl: 'https://i.ytimg.com/an_webp/XSs2cR2Tvuk/mqdefault_6s.webp?du=3000&sqp=CJKGmoEG&rs=AOn4CLA9biSBsXCUALBDPmWfqxxpyZIC-A',
         videoUrl: 'https://youtu.be/XSs2cR2Tvuk',
         title: 'Wake Me Up ',
         description: 'Avicii (violin/cello/bass cover) - Simply Three'
     },
     {//2
-        imgUrl: 'https://i.ytimg.com/an_webp/TdYUQ3gOrvQ/mqdefault_6s.webp?du=3000&sqp=COilmIEG&rs=AOn4CLCS-cFXliIlbey-Q_0T8xbBDL7jWg',
+        imgUrl: 'https://i.ytimg.com/an_webp/TdYUQ3gOrvQ/mqdefault_6s.webp?du=3000&sqp=CJSVmoEG&rs=AOn4CLCBFAXpE1lY86ci4Qv3DXSqqMYs-w',
         videoUrl: 'https://youtu.be/TdYUQ3gOrvQ',
         title: 'Форсаж 9',
         description: '💥 Русский Супербоул-ролик + Трейлер 💥'
     },
     {
-        imgUrl: 'https://i.ytimg.com/an_webp/9KJxaFHotqI/mqdefault_6s.webp?du=3000&sqp=CNz7l4EG&rs=AOn4CLAiRo5NLOd1tZ9LQndLw9BmkUqYHQ',
+        imgUrl: 'https://i.ytimg.com/an_webp/9KJxaFHotqI/mqdefault_6s.webp?du=3000&sqp=CKS0moEG&rs=AOn4CLCkKgaS5TjGmvef6OyJsAFVCdga7g',
         videoUrl: 'https://youtu.be/9KJxaFHotqI',
         title: 'React Hooks',
         description: 'Полный Курс (Про Все Хуки!)'
     },
     { //4
-        imgUrl: 'https://i.ytimg.com/an_webp/CxwJrzEdw1U/mqdefault_6s.webp?du=3000&sqp=CK-GmIEG&rs=AOn4CLDldjUPV8ws5-wwW__cKANuguB9QQ',
+        imgUrl: 'https://i.ytimg.com/an_webp/CxwJrzEdw1U/mqdefault_6s.webp?du=3000&sqp=CIORmoEG&rs=AOn4CLA5WJvkU_vBKE6Ux-3aLGzAYlpc2g',
         videoUrl: 'https://youtu.be/CxwJrzEdw1U',
         title: 'Norway 4K ',
         description: 'Scenic Relaxation Film with Calming Music'
@@ -30,13 +30,13 @@ const data = [
         description: 'Purified Radio Episode 233'
     },
     {//6
-        imgUrl: 'https://i.ytimg.com/an_webp/ZS3O0OOn0a0/mqdefault_6s_480x270.webp?du=3000&sqp=COinmIEG&rs=AOn4CLDkmYr7Lv9bqsfYnFz2tWGaO4aO0A',
+        imgUrl: 'https://i.ytimg.com/an_webp/ZS3O0OOn0a0/mqdefault_6s.webp?du=3000&sqp=CNDCmoEG&rs=AOn4CLDQ3I2IqTinaS_yQ6xJw9E5StMe0A',
         videoUrl: 'https://youtu.be/ZS3O0OOn0a0',
         title: 'Table Tennis',
         description: 'The Funniest Table Tennis Match in HISTORY'
     },
     {//7
-        imgUrl: 'https://i.ytimg.com/an_webp/hEmCLBOoYr8/mqdefault_6s.webp?du=3000&sqp=CJSQmIEG&rs=AOn4CLCsCrzWh_S58ZMhM6VxUY9PBIgkZw',
+        imgUrl: 'https://i.ytimg.com/an_webp/hEmCLBOoYr8/mqdefault_6s.webp?du=3000&sqp=CKH7mYEG&rs=AOn4CLAyL48b5CIq5b80IyhRJXCoFNhnhQ',
         videoUrl: 'https://youtu.be/hEmCLBOoYr8',
         title: 'Актеры дубляжа',
         description: 'Актеры дубляжа в реальной жизни снова в машине!'
