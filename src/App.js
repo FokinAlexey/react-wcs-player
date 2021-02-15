05937f4987c8358f4ecfd75294b95de7d0e92cef
FokinAlexey <a.fokin1979@gmail.com>
2021-02-15 23:48:51 +0300

    add like and dislike counter

--- src/App.js
+++ src/App.js
@@ -3,25 +3,48 @@ import { Component } from 'react';
 import CardList from './Components/CardList';
 import Player from './Components/Player';
 import SearchBar from "./Components/searchbar";
+import data from './Components/data';
 
 
-class App extends Component{
-  render(){
-     return (
-    <div >
-      <h1> Wild-Code-School Player</h1>
-      <SearchBar/>
-      <div className="App">
-        <Player />
-        <CardList />
-      </div>
-
-
-      {/* <Main /> */}
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tern: '',
+      title: props.title,
+    }
   }
- 
+
+
+  search(items, tern) {
+    if (tern.length === 0) {
+      return items
+    }
+    items.filter((item) => {
+      return item.label.indexOf(tern) > -1
+    });
+  }
+
+  render() {
+    const { title, tern } = this.state
+    const visibleItems = this.search(title, tern);
+
+    // const filter = title.filter(element => element ==)
+    return (
+      <div>
+        <h1> Wild-Code-School Player</h1>
+        <SearchBar />
+        <div className="App">
+          <Player />
+          <CardList />
+        </div>
+
+
+        {/* <Main /> */}
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,19 +1,37 @@
 import ReactPlayer from 'react-player/youtube';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class  Player extends Component{
-    render(){
-       return (
-        <div className = "player"> 
-              <ReactPlayer url='https://youtu.be/TdYUQ3gOrvQ' />
-              <h2 className ="title"> F9 the fast saga</h2>
-              <p className= "descript">Fast & Furious 9, 2021</p>
-        </div>
-      
-
-    ); 
+class Player extends Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0,
+            count1: 0
+        }
     }
-    
+
+    handelClick() {
+        this.setState({ count: this.state.count + 1 })
+    }
+
+    handelClick1() {
+        this.setState({ count1: this.state.count1 + 1 })
+    }
+
+    render() {
+        return (
+            <div className="player">
+                <ReactPlayer url='https://youtu.be/TdYUQ3gOrvQ' />
+                <h2 className="title"> F9 the fast saga</h2>
+                <p className="descript">Fast & Furious 9, 2021</p>
+                <span onClick={() => this.handelClick()}> 👍 {this.state.count}</span>
+                <span onClick={() => this.handelClick1()} > 👎 {this.state.count1}</span>
+            </div>
+
+
+        );
+    }
+
 }
 
 
